2d36926d8274e8414d7099f96f40ef89cf482e76
LIK WAI NG <nglikwai@ymail.com>
2023-04-12 17:42:18 +0800

    fix: cors

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
+module.exports = {
+  async headers() {
+    return [
+      {
+        // Set CORS headers
+        source: '/api/:path*',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*', // Allow any origin
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET, POST, PUT, DELETE, OPTIONS', // Allow any HTTP method
+          },
+        ],
+      },
+    ];
+  },
+};
